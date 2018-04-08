aca7d5790365520e19b103532dd07a86e59ea32f
André Vaccari <andrevaccari95@gmail.com>
2018-04-08 12:57:50 -0300

    Cors and Helmet added

--- config/app.js
+++ config/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const knex = require('../db/knex')
+const helmet = require('helmet')
+const cors = require('cors')
 
 const app = express()
+
+app.use(helmet())
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({
